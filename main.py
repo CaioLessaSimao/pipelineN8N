7a0a3532c687db7f4198ab0b4bd2f99398113dc4
Lessa <87532378+CaioLessaSimao@users.noreply.github.com>
2025-04-16 13:50:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Pipeline:
         # This is where you can add your custom pipelines like RAG.
         try:
            print("Entrou no try")
-           pprint(body["files"][0]["file"]["data"]["content"])
+           pprint("Esse é o conteudo", body["files"][0]["file"]["data"]["content"])
            if(body["files"]):
                print("Entrou no if")
                return "jorge"
